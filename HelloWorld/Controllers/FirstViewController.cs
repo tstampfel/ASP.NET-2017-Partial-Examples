1d1bc1739cf8e4dfaec4fee64acd50b6183cf41d
Tomislav Stampfel <tomislav.stampfel@affecto.com>
2017-10-20 09:57:12 +0200

    Added changes

---
+++ HelloWorld/Controllers/FirstViewController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class FirstViewController : Controller
+    {
+        // GET: FirstView
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MyFirstView()
+
+        { 
+
+
+
+
+            ViewBag.MyTitle = "Hello from my View Bag!";
+
+            return View();
+        }
+    }
+}
---
+++ HelloWorld/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "Hello World!";
+        }
+
+        public string Hello()
+        {
+            return "<h1> Hello from Html! <h1/>";
+        }
+
+
+    }
+}
