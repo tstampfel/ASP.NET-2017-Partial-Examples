5101f44bdb858ddbef8bec884b5d5537d7e0316d
Tomislav Stampfel <tomislav.stampfel@affecto.com>
2017-10-25 09:45:53 +0200

    Simple Form Added

---
+++ SimpleValidation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SimpleValidation.MvcApplication" Language="C#" %>
---
+++ SimpleValidation/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SimpleValidation.Models
+{
+    public class User
+    {   
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+        public string  Email { get; set; }
+        [Required]
+        [Compare("Email", ErrorMessage = "The email and confirmation do not match.")]
+        [Display(Name = "Confirm Email")]
+        public string ConfirmEmail { get; set; }
+
+
+
+    }
+}
