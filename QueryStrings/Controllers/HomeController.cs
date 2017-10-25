6b15544627acbcc5eb2cb7f2a004159a8e7f2561
Tomislav Stampfel <tomislav.stampfel@affecto.com>
2017-10-25 13:21:07 +0200

    Added QueryString

---
+++ QueryStrings/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using QueryStrings.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QueryStrings.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Search(string searchTerm)
+        {
+            Cars cars = new Cars();
+            var model = cars.ReturnCars().Where(x => x.Name.StartsWith(searchTerm)).ToList();
+            return View(model);
+        }
+    }
+}
---
+++ QueryStrings/Models/Cars.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QueryStrings.Models
+{
+    public class Cars
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+
+        public int Power { get; set; }
+
+
+        public List<Cars> CarsList = new List<Cars>();
+
+        public List<Cars> ReturnCars()
+        {
+            CarsList.Add( new Cars { Id = 1, Name = "Volvo", Country = "Sweden", Power=200 });
+            CarsList.Add(new Cars { Id = 2, Name = "BMW", Country = "Germany", Power = 300 });
+            CarsList.Add(new Cars { Id = 3, Name = "Audi", Country = "Germany", Power = 400 });
+            CarsList.Add(new Cars { Id = 4, Name = "Ford", Country = "USA", Power = 500 });
+            CarsList.Add(new Cars { Id = 2, Name = "Bently", Country = "UK", Power = 300 });
+
+            return CarsList;
+        }
+
+
+
+    }
+}
